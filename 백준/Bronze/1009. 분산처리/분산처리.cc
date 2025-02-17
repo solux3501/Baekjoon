15d99855ff66130238ff83de179e8b58c050b027
solux3501 <70375441+solux3501@users.noreply.github.com>
2025-02-17 20:17:31 +0900

    [Bronze II] Title: 분산처리, Time: 748 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) 
+
+{
+
+    int num;
+
+    cin >> num;
+
+    
+
+    for (int i=0;i<num;i++)
+
+    {
+
+        int a, b, result=1;
+
+        cin >> a >> b;
+
+        
+
+        for (int j=0;j<b;j++)
+
+        {
+
+            result = (result*a)%10;
+
+        }
+
+        if (result==0)
+
+            cout << 10 <<endl;
+
+        else     
+
+            cout << result <<endl;
+
+    }
+
+    return 0;
+
+}
