a281540bb257453f06fa9853f4f60c7bbe819356
solux3501 <70375441+solux3501@users.noreply.github.com>
2025-05-27 23:02:07 +0900

    [Bronze I] Title: Honey and Milk Land, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/7466. Honey and Milk Land/Honey and Milk Land.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+
+#include <vector>
+
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{
+
+    int N, M, tmp;
+
+    int result;
+
+    cin >> N >> M;
+
+    vector <int> x(1);
+
+    vector <int> y(1);
+
+    
+
+    for (int i=1;i<N;i++)
+
+    {
+
+        cin >> tmp;
+
+        x.push_back(tmp+x[i-1]);
+
+        //cout << x[i] << '\n';
+
+    }
+
+    
+
+    for(int i=1;i<M;i++)
+
+    {
+
+        cin >> tmp;
+
+        y.push_back(tmp+y[i-1]);
+
+    }
+
+    
+
+    result = ceil(sqrt(pow(x[N-1],2) +pow(y[M-1],2)));   
+
+    cout  << result;
+
+    
+
+    return 0;
+
+}
