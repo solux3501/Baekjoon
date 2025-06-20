6cb6cf34cc316364173cd394399aa2ac056e1215
solux3501 <70375441+solux3501@users.noreply.github.com>
2025-06-20 21:20:20 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (j < N - i - 1)
+				cout << " ";
+			else
+				cout << "*";
+		}
+		cout << '\n';
+	}
+
+
+	return 0;
+}
