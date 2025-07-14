cc97816b3bca152207b8736d4c8cb30ec1a41251
solux3501 <70375441+solux3501@users.noreply.github.com>
2025-07-14 19:05:40 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string sen;
+	cin >> sen;
+
+	cout << ":fan::fan::fan:" << '\n';
+	cout << ":fan::" << sen << "::fan:" << '\n';
+	cout << ":fan::fan::fan:" << '\n';
+
+	return 0;
+}
